5865b3fd344bd430545c21741367d44a789fa52b
Bhuvana Teja Kanakam <116140522+bhuvanakanakam@users.noreply.github.com>
2023-11-18 22:16:45 +0530

    Update main.c

--- Min-Max/main.c
+++ Min-Max/main.c
@@ -1,16 +1,24 @@
 #include <inttypes.h>
 
-int64_t minimum(int64_t* arr, int64_t n);
-int64_t maximum(int64_t* arr, int64_t n);
+extern int64_t minimum(int64_t* arr, int64_t n);
+extern int64_t maximum(int64_t* arr, int64_t n);
 
 int main() {
-    int64_t n;  // no of elements in the array
-    scanf("%ld",&n); 
+    int64_t n;  // number of elements in the array
+    scanf("%" SCNd64, &n); 
     int64_t arr[n];
-    for (int64_t i=0; i<n; i++) {
-        scanf("%ld",&arr[i]);
+    
+    // Input array elements
+    for (int64_t i = 0; i < n; i++) {
+        scanf("%" SCNd64, &arr[i]);
     }
+    
+    // Call the assembly functions
     int64_t minElem = minimum(arr, n);
     int64_t maxElem = maximum(arr, n);
-    printf("Minimum element in array: %ld \nMaximum element in array: %ld\n", minElem, maxElem);
+    
+    // Print the results
+    printf("Minimum element in array: %" PRId64 "\nMaximum element in array: %" PRId64 "\n", minElem, maxElem);
+
+    return 0;
 }
