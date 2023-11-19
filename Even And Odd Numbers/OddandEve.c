973ec43dee0e37eb70c7c39fca5842d5562052d6
Bhuvana Teja Kanakam <116140522+bhuvanakanakam@users.noreply.github.com>
2023-11-19 16:29:54 +0530

    Update OddandEven.c

--- Even And Odd Numbers/OddandEve.c
+++ Even And Odd Numbers/OddandEve.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 #include<inttypes.h>
 
+int64_t numEven(int64_t[], uint64_t);
 int64_t numOdd(int64_t[], uint64_t);
-int main(){
-    long int a[]={10,20,90,2250,60,12340,77,99,12,-4,0,56};
 
-    printf("\nOdd Elements: %ld\n",numOdd(a,12));
+int main() {
+    int64_t a[] = {10, 20, 90, 2250, 60, 12340, 77, 99, 12, -4, 0, 56};
+    uint64_t size = 12;
+
+    printf("\nEven Elements: %ld\n", numEven(a, size));
+    printf("Odd Elements: %ld\n", numOdd(a, size));
+
     return 0;
 }
