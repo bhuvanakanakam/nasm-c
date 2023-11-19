1ed1ffe779451c66c1e8092b7da0c68ac66401cc
Bhuvana Teja Kanakam <116140522+bhuvanakanakam@users.noreply.github.com>
2023-11-19 17:29:49 +0530

    c code for (a^2-b^2)

--- Arithematic/square.c
+++ Arithematic/square.c
@@ -3,6 +3,6 @@
 int64_t sq_diff(int64_t,int64_t);
 int main(){
 	long int a =13,b=12;
-	printf("result is %ld\n",sq_diff(a,b));
+	printf("Result for calculating a^2-b^2 is %ld\n",sq_diff(a,b));
 	return 0;
 }
