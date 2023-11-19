3210e81b78f5775acca3542629b351606bea9513
Bhuvana Teja Kanakam <116140522+bhuvanakanakam@users.noreply.github.com>
2023-11-19 17:30:51 +0530

    Number of Odd and Even Elements in the Array

--- Even And Odd Numbers/OddandEve.c
+++ Even And Odd Numbers/OddandEve.c
@@ -8,8 +8,8 @@ int main() {
     int64_t a[] = {10, 20, 90, 2250, 60, 12340, 77, 99, 12, -4, 0, 56};
     uint64_t size = 12;
 
-    printf("\nEven Elements: %ld\n", numEven(a, size));
-    printf("Odd Elements: %ld\n", numOdd(a, size));
+    printf("\nNumber of Even Elements in the Array: %ld\n", numEven(a, size));
+    printf("Number of Odd Elements in the Array: %ld\n", numOdd(a, size));
 
     return 0;
 }
