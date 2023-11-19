bf9ee857a5c230924b349f8a4fa33dbe79bf999c
Bhuvana Teja Kanakam <116140522+bhuvanakanakam@users.noreply.github.com>
2023-11-19 17:32:08 +0530

    Number of Odd Numbers

--- Even And Odd Numbers/noOfOdd.asm
+++ Even And Odd Numbers/noOfOdd.asm
@@ -1,21 +1,24 @@
-global numOdd
-section .text
-numOdd:
-    xor rax, rax
-    cmp rsi, 0
-    je finish
+; Function to count the number of odd numbers in an array
 
-next1:
-    mov rcx, [rdi]
-    and rcx, 1
-    jnz next2
-    inc rax
+global numOdd          ; Declare the function as global with the name 'numOdd'
+section .text          ; Start the text section
 
-next2:
-    dec rsi
-    jz finish
-    add rdi, 8
-    jmp next1
+numOdd:                ; Label for the numOdd function
+    xor rax, rax       ; Clear the accumulator rax (initialize to zero)
+    cmp rsi, 0          ; Compare the second parameter (array size) with 0
+    je finish           ; If the array size is 0, jump to the finish label
 
-finish:
-    ret
+next1:                  ; Label for the first loop
+    mov rcx, [rdi]      ; Load the value at the memory address in rdi into rcx (assume 8-byte integers)
+    and rcx, 1          ; Perform bitwise AND with 1 to check if the number is odd (if the result is 1, it's odd)
+    jnz next2           ; Jump to next2 if the number is odd
+    inc rax             ; Increment the count of odd numbers
+
+next2:                  ; Label for the second part of the loop
+    dec rsi             ; Decrement the array size
+    jz finish           ; If the array size is 0, jump to the finish label
+    add rdi, 8          ; Move to the next 8-byte element in the array (assuming 8-byte integers)
+    jmp next1           ; Jump back to next1 to continue the loop
+
+finish:                 ; Label for the end of the function
+    ret                ; Return from the function
