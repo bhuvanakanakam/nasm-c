0dd3d2f755af78e782941c64e64693ffb08142f1
Bhuvana Teja Kanakam <116140522+bhuvanakanakam@users.noreply.github.com>
2023-11-19 17:29:12 +0530

    a^2 - b^2

--- Arithematic/square.asm
+++ Arithematic/square.asm
@@ -1,33 +1,32 @@
-; calculates the a^2-b^2 
+; Function to calculate a^2 - b^2
 
-global sq_diff         ; 
+global sq_diff         ; Declare the function as global with the name 'sq_diff'
+section .text          ; Start the text section
 
-section .text          ; 
+sq_diff:               ; Label for the sq_diff function
+  mov rax, rdi         ; Move the value in rdi to rax (copying the first input parameter)
+  mov rbx, rsi         ; Move the value in rsi to rbx (copying the second input parameter)
 
-sq_diff:               ; Defines the label for the sq_diff function
-  mov rax, rdi         ; Moves the value in rdi to rax (copying the first input parameter)
-  mov rbx, rsi         ; Moves the value in rsi to rbx (copying the second input parameter)
+  xor rcx, rcx         ; Clear the value in rcx (initialize to zero)
+  xor rdx, rdx         ; Clear the value in rdx (initialize to zero)
 
-  xor rcx, rcx         ; Clears the value in rcx (initialize to zero)
-  xor rdx, rdx         ; Clears the value in rdx (initialize to zero)
+  mov rcx, 1           ; Set rcx to 1 (initialize the counter for the first loop)
+  mov rdx, 1           ; Set rdx to 1 (initialize the counter for the second loop)
 
-  mov rcx, 1           ; Sets rcx to 1 (initialize the counter for the loop)
-  mov rdx, 1           ; Sets rdx to 1 (initialize the counter for the loop)
-
-next:                  ; Label for the loop
-  add rax, rdi         ; Adds the value in rdi to rax (incrementing rax by the value of rdi)
-  inc rcx              ; Increments the value in rcx (incrementing the loop counter)
-  cmp rcx, rdi         ; Compares rcx and rdi
-  jne next             ; Jumps to the 'next' label if rcx is not equal to rdi
+next:                  ; Label for the first loop
+  add rax, rdi         ; Add the value in rdi to rax (incrementing rax by the value of rdi)
+  inc rcx              ; Increment the value in rcx (incrementing the loop counter)
+  cmp rcx, rdi         ; Compare rcx and rdi
+  jne next             ; Jump to the 'next' label if rcx is not equal to rdi
 
 next2:                 ; Label for the second loop
-   add rbx, rsi        ; Adds the value in rsi to rbx (incrementing rbx by the value of rsi)
-   inc rdx             ; Increments the value in rdx (incrementing the loop counter)
-   cmp rdx, rsi        ; Compares rdx and rsi
-   jne next2           ; Jumps to the 'next2' label if rdx is not equal to rsi
+   add rbx, rsi        ; Add the value in rsi to rbx (incrementing rbx by the value of rsi)
+   inc rdx             ; Increment the value in rdx (incrementing the loop counter)
+   cmp rdx, rsi        ; Compare rdx and rsi
+   jne next2           ; Jump to the 'next2' label if rdx is not equal to rsi
 
 trans:                 ; Label for the subtraction operation
-   sub rax, rbx        ; Subtracts the value in rbx from rax (calculating the square difference)
+   sub rax, rbx        ; Subtract the value in rbx from rax (calculating the square difference)
 
 finish:                ; Label for the end of the function
-   ret                 ; Returns from the function
+   ret                 ; Return from the function
