8cde78cb13624a8a52ec9c9e8273a28943344ce8
Bhuvana Teja Kanakam <116140522+bhuvanakanakam@users.noreply.github.com>
2023-11-25 12:50:20 +0530

    Median

--- Median/median.asm
+++ Median/median.asm
@@ -1,54 +1,54 @@
 global medianN                 
 section .text
 
-medianN:                        ; Start of the medianN function
+medianN:                        
     mov rax, [rdi];             ; Move the value at the memory address stored in rdi to rax
-    cmp rsi, $1;                 ; Compare rsi with 1
-    je finish;                   ; If equal, jump to finish
+    cmp rsi, $1;                ; Compare rsi with 1
+    je finish;                  ; If equal, jump to finish
 
-next:                           ; Label for the next section of code
+next:                           
     mov rbx, rsi;               ; Move the value of rsi to rbx
-    test rbx, 1;                 ; Test if rbx is odd
+    test rbx, 1;                ; Test if rbx is odd
     jz even;                    ; If rbx is even, jump to even
-    jnz odd;                     ; If rbx is odd, jump to odd
+    jnz odd;                    ; If rbx is odd, jump to odd
 
-odd:                            ; Code for odd length array
+odd:                            
     mov rax, rsi;               ; Move the value of rsi to rax
-    add rax, 1;                  ; Add 1 to rax
-    mov rbx, 2;                  ; Move the value 2 to rbx
-    xor rdx, rdx;                ; Clear rdx
-    div rbx;                     ; Divide rax by rbx, result in rax, remainder in rdx
-    mov rcx, rax;                ; Move the quotient to rcx
+    add rax, 1;                 ; Add 1 to rax
+    mov rbx, 2;                 ; Move the value 2 to rbx
+    xor rdx, rdx;               ; Clear rdx
+    div rbx;                    ; Divide rax by rbx, result in rax, remainder in rdx
+    mov rcx, rax;               ; Move the quotient to rcx
     
-    add rsi, 1;                  ; Increment rsi
+    add rsi, 1;                 ; Increment rsi
 
-getelement:                     ; Loop to get elements from the array
-    mov rax, [rdi];              ; Move the value at the memory address stored in rdi to rax
-    add rdi, $8;                 ; Move to the next element in the array
-    dec rsi;                     ; Decrement rsi
-    cmp rsi, rcx;                ; Compare rsi with rcx
-    jne getelement;              ; If not equal, jump to getelement
-    je finish;                   ; If equal, jump to finish
+getelement:                     
+    mov rax, [rdi];             ; Move the value at the memory address stored in rdi to rax
+    add rdi, $8;                ; Move to the next element in the array
+    dec rsi;                    ; Decrement rsi
+    cmp rsi, rcx;               ; Compare rsi with rcx
+    jne getelement;             ; If not equal, jump to getelement
+    je finish;                  ; If equal, jump to finish
 
-even:                           ; Code for even length array
-    mov rax, rsi;                ; Move the value of rsi to rax
-    mov rbx, $2;                 ; Move the value 2 to rbx
-    xor rdx, rdx;                ; Clear rdx
-    div rbx;                     ; Divide rax by rbx, result in rax, remainder in rdx
-    mov rcx, rax;                ; Move the quotient to rcx
+even:                           
+    mov rax, rsi;               ; Move the value of rsi to rax
+    mov rbx, $2;                ; Move the value 2 to rbx
+    xor rdx, rdx;               ; Clear rdx
+    div rbx;                    ; Divide rax by rbx, result in rax, remainder in rdx
+    mov rcx, rax;               ; Move the quotient to rcx
 
-getelementeven:                 ; Loop to get elements from the array for even length
-    dec rsi;                     ; Decrement rsi
-    mov rax, [rdi];              ; Move the value at the memory address stored in rdi to rax
-    add rdi, $8;                 ; Move to the next element in the array
-    cmp rsi, rcx;                ; Compare rsi with rcx
-    jne getelementeven;          ; If not equal, jump to getelementeven
-    je addnextelement;           ; If equal, jump to addnextelement
+getelementeven:                 
+    dec rsi;                    ; Decrement rsi
+    mov rax, [rdi];             ; Move the value at the memory address stored in rdi to rax
+    add rdi, $8;                ; Move to the next element in the array
+    cmp rsi, rcx;               ; Compare rsi with rcx
+    jne getelementeven;         ; If not equal, jump to getelementeven
+    je addnextelement;          ; If equal, jump to addnextelement
 
-addnextelement:                 ; Code to add the next element for even length
-    add rax, [rdi];              ; Add the next element in the array to rax
-    mov rbx, $2;                 ; Move the value 2 to rbx
-    div rbx;                     ; Divide rax by rbx, result in rax, remainder in rdx
+addnextelement:                 
+    add rax, [rdi];             ; Add the next element in the array to rax
+    mov rbx, $2;                ; Move the value 2 to rbx
+    div rbx;                    ; Divide rax by rbx, result in rax, remainder in rdx
 
-finish:                         ; Finish of the function
-    ret                          ; Return from the function
+finish:                         
+    ret                         ; Return from the function
